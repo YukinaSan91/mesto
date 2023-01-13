68bc52523fdc828b994f5ccb8d1508326979139e
Yuliya Bespomestnaya <yukina91@mail.ru>
2023-01-14 01:24:12 +0300

    ПР4

---
+++ scripts/index.js
@@ -0,0 +1,41 @@
+let popupElement = document.querySelector('.popup');
+let popupCloseButtonElement = popupElement.querySelector('.popup__close');
+let popupOpenButtonElement = document.querySelector('.profile__popup-open');
+let formElement = popupElement.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__text_type_name');
+let jobInput = formElement.querySelector('.popup__text_type_job');
+let popupSaveButtonElement = formElement.querySelector('.popup__save-button');
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__text');
+
+
+// Открытие попапа
+let popupOpen = function() {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileJob.textContent;
+}
+
+popupOpenButtonElement.addEventListener('click', popupOpen);
+
+
+// Закрытие попапа
+let popupClose = function() {
+  popupElement.classList.remove('popup_opened');
+}
+
+popupCloseButtonElement.addEventListener('click', popupClose);
+
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupClose();
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
