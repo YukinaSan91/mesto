70b5fe5169d35db4dc9a3c4e0b8c275f7e024d5a
Yuliya Bespomestnaya <yukina91@mail.ru>
2023-02-01 21:16:49 +0300

    ПР5

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,88 @@
-let popupElement = document.querySelector('.popup');
-let popupCloseButtonElement = popupElement.querySelector('.popup__close');
-let popupOpenButtonElement = document.querySelector('.profile__popup-open');
-let formElement = popupElement.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__text_type_name');
-let jobInput = formElement.querySelector('.popup__text_type_job');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__text');
+//Попапы
+const popupElement = document.querySelector('.popup');
+const popupAddElement = document.querySelector('.popup_type_add');
+const popupImgElement = document.querySelector('.popup_type_image');
+
+//Кнопки для управления попапами
+const popupCloseButtonElement = document.querySelectorAll('.popup__close');
+const popupAddButtonElement = document.querySelector('.profile__add-button');
+const popupOpenButtonElement = document.querySelector('.profile__popup-open');
+const popupSaveButtonElement = document.querySelector('.popup__save-button');
+
+//Формы
+const formElement = popupElement.querySelector('.popup__form');
+const nameInput = formElement.querySelector('.popup__text_type_name');
+const jobInput = formElement.querySelector('.popup__text_type_job');
+
+const profileName = document.querySelector('.profile__title');
+const profileJob = document.querySelector('.profile__text');
+
+const formAddElement = document.querySelector('.popup__form_type_add');
+const nameAddInput = formAddElement.querySelector('.popup__text_type_title');
+const urlAddInput = formAddElement.querySelector('.popup__text_type_url');
+
+//Темплейты
+const cardsList = document.querySelector('.elements');
+const cardTemplate = document.querySelector('#card-template').content.querySelector('.element');
+
 
 
 // Открытие попапа
-let popupOpen = function() {
+const popupOpen = function() {
   popupElement.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
-}
+};
+
+const openPopup = function() {
+  popupAddElement.classList.add('popup_opened');
+};
+
+const openImgPopup = function() {
+  popupImgElement.classList.add('popup_opened');
+};
 
 // Закрытие попапа
-let popupClose = function() {
-  popupElement.classList.remove('popup_opened');
-}
+const popupClose = function(evt) {
+   evt.target.closest('.popup').classList.remove('popup_opened');
+};
+
+popupCloseButtonElement.forEach((CloseButton) => {
+  CloseButton.addEventListener('click', popupClose);
+});
+
+//Функция карточки
+function renderCards (initialCards) {
+  const cards = initialCards.map((item) => {
+    const card = cardTemplate.cloneNode(true);
+    card.querySelector('.element__text').textContent = item.name;
+    card.querySelector('.element__image').src = item.link;
+    card.querySelector('.element__image').alt = item.name;
+
+    const cardImgElement = document.querySelector('.popup__image');
+    const cardAltElement = document.querySelector('.popup__image');
+    const cardTitleElement = document.querySelector('.popup__title');
+
+    card.querySelector('.element__image').addEventListener('click', function() {
+      openImgPopup();
+      cardImgElement.src = item.link;
+      cardAltElement.alt = item.name;
+      cardTitleElement.textContent = item.name;
+    });
+
+    card.querySelector('.element__del-button').addEventListener('click', (evt) => {
+      evt.target.closest('.element').remove()});
+    card.querySelector('.element__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('element__like-button_active')});
+
+    return card;
+  });
+
+  cardsList.prepend(...cards);
+  };
+
+renderCards(initialCards);
+
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -26,12 +90,27 @@ function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
-  popupClose();
-}
+  popupClose(evt);
+};
+
+function handleFormAddSubmit (evt) {
+  evt.preventDefault();
+
+  const addCard = [{
+    name: nameAddInput.value,
+    link: urlAddInput.value,
+  }];
+
+  renderCards(addCard);
+  popupClose(evt);
+};
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+formAddElement.addEventListener('submit', handleFormAddSubmit);
 
+//Слушатели событий
 popupOpenButtonElement.addEventListener('click', popupOpen);
-popupCloseButtonElement.addEventListener('click', popupClose);
+popupAddButtonElement.addEventListener('click', openPopup);
+
---
+++ scripts/massiv.js
@@ -0,0 +1,27 @@
+// Динамичный контент, массив первоначальных карточек
+const initialCards = [
+  {
+    name: 'Карачаевск',
+    link: 'http://rasfokus.ru/images/photos/medium/8ffeea8cd0355dfd6af9e75d0914f154.jpg'
+  },
+  {
+    name: 'Эльбрус',
+    link: 'https://kartinkin.net/pics/uploads/posts/2022-08/1659588387_27-kartinkin-net-p-vid-na-elbrus-s-chegeta-priroda-krasivo-fo-27.jpg'
+  },
+  {
+    name: 'Домбай',
+    link: 'https://cdn.fishki.net/upload/post/201406/27/1280912/3d64003ea6e5aafa6cfd1e548c89538d.jpg'
+  },
+  {
+    name: 'Алтай',
+    link: 'https://key-ms.ru/wp-content/uploads/6/c/3/6c3d435b28ce23de675d85a0638c9b7c.jpeg'
+  },
+  {
+    name: 'Бийск',
+    link: 'https://cdn.culture.ru/images/fbe282a1-25b5-5954-acd9-c04b61d61db1'
+  },
+  {
+    name: 'Шато Эркен',
+    link: 'https://33ways.ru/wp-content/uploads/2022/05/DSC07552-scaled.jpg'
+  }
+];
